ba4656bc779cca15c88743a4e4ad486ba4b0823f
codeMin <code.min657@gmail.com>
2023-12-12 11:54:57 +0900

    commit

--- src/pages/Project.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-function Project(){
-    return(
-      <div>PROJECT</div>
-    )
-  }
-  export default Project;
--- src/pages/SkillStack.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-function SkillStack(){
-  return(
-    <div>SKILL</div>
-  )
-}
-export default SkillStack;
